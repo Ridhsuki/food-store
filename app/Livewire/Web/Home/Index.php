5c6f60d20417bce9dd563541ae10a8390308f5c0
Ridhsuki <basukiridhoalghifary@gmail.com>
2025-03-12 23:41:28 +0700

    feat home: populer product Langkah 1 - Get Data Products Populer di Class Component Langkah 2 - Membuat Component Card Product Popular Langkah 3 - Menampilkan Data Products Populer Langkah 4 - Uji Coba Menampilkan Data Products Populer

--- app/Livewire/Web/Home/Index.php
+++ app/Livewire/Web/Home/Index.php
@@ -3,11 +3,21 @@
 namespace App\Livewire\Web\Home;
 
 use App\Models\Slider;
+use App\Models\Product;
 use Livewire\Component;
 use App\Models\Category;
 
 class Index extends Component
-{
+{    
+    protected function getPopularProducts()
+    {
+        return Product::with('category', 'ratings.customer')
+            ->withAvg('ratings', 'rating') // Menghitung rata-rata rating
+            ->having('ratings_avg_rating', '>=', 4)
+            ->limit(5)
+            ->get();
+    }
+
     public function render()
     {
         return view('livewire.web.home.index', [
@@ -17,6 +27,9 @@ class Index extends Component
 
             //get categories
             'categories' => Category::latest()->get(),
+
+            //get popular products
+            'popularProducts' => $this->getPopularProducts(),
             
         ]);
     }
