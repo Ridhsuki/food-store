4ff56a21f75ccd94f8e5c502d38195a9c4c05505
Ridhsuki <basukiridhoalghifary@gmail.com>
2025-03-12 21:55:47 +0700

    feat my order Langkah 1 - Membuat Component My Orders Langkah 2 - Membuat Route My Orders Index Langkah 3 - Get Data Transaksi di Class Component Langkah 4 - Menampilkan Data My Orders / Transactions Langkah 5 - Publish View Pagination Langkah 6 - Custom Pagination Laravel Langkah 7 - Uji Coba Menampilkan Halaman My Orders

--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/register', Auth\Register::class)->name('register');
 //route login
 Route::get('/login', Auth\Login::class)->name('login');
+//route group account
+Route::middleware('auth:customer')->group(function () {
+
+    Route::group(['prefix' => 'account'], function () {
+
+        //route my order
+        Route::get('/my-orders', Account\MyOrders\Index::class)->name('account.my-orders.index');
+
+    });
+
+});
