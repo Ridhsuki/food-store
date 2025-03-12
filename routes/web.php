b99ebe380bba87523092bfa5d56dc0efd2292261
Ridhsuki <basukiridhoalghifary@gmail.com>
2025-03-12 21:42:05 +0700

    feat authentication: component register
    Langkah 1 - Membuat Component Button Back
    Langkah 2 - Membuat Component Register
    Langkah 3 - Membuat Route Register
    Langkah 4 - Membuat Fungsi Register
    Langkah 5 - Membuat Form Register
    Langkah 6 - Uji Coba Proses Register

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
+namespace App\Livewire;
+
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//route register
+Route::get('/register', Auth\Register::class)->name('register');
