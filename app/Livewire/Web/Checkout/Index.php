6b6cf439988b068cfaf1fbc684b245605f4d2a98
Ridhsuki <basukiridhoalghifary@gmail.com>
2025-03-15 15:16:29 +0700

    feat checkout page: menampilkan data provinsi - Langkah 1 - Get Data Provinsi di Class Component - Langkah 2 - Menampilkan Data Provinsi - Langkah 3 - Uji Coba Menampilkan Data Provinsi

--- app/Livewire/Web/Checkout/Index.php
+++ app/Livewire/Web/Checkout/Index.php
@@ -3,11 +3,19 @@
 namespace App\Livewire\Web\Checkout;
 
 use Livewire\Component;
+use App\Models\Province;
 
 class Index extends Component
 {
+    public $address;
+    public $province_id;
+
+
     public function render()
     {
-        return view('livewire.web.checkout.index');
+        //get provinces
+        $provinces = Province::query()->get();
+
+        return view('livewire.web.checkout.index', compact('provinces'));
     }
 }
